d4d075d15ec8261ac50231d7301e511f39c80d2d
Jerel Crespo <mrc220@live.com>
2017-02-04 16:08:01 -0500

    pulling mimis merge

--- db/migrations/20170203171421-create-categories.js
+++ db/migrations/20170203171421-create-categories.js
@@ -11,9 +11,6 @@ module.exports = {
       name: {
         type: Sequelize.STRING(50)
       },
-      tech_id: {
-        type: Sequelize.STRING
-      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
