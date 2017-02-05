245fe3a489df510f07301d95ca11b8be2f912edf
matt kersner <mattkersner@gmail.com>
2017-02-05 14:17:06 -0500

    working on rendering reviews to tech details page

--- routes/reviews.js
+++ routes/reviews.js
@@ -4,6 +4,7 @@ const models = require('../db/models/index');
 
 
 //get route to render the review form
+//using params to pass the tech_id to the db
 router.get('/:id', function(req, res) {
   res.render('review/reviewform', {tech_id: req.params.id});
 })
@@ -18,9 +19,8 @@ router.post('/', function(req, res) {
     review_text: req.body.review_text,
     title: req.body.title
   }).then(() => {
-    //where should I redirect? Have to add a loop to render comments on
-    //specific tech details page
-    res.redirect('/user')
+    //redirect to the specific technology page where the id will be posted
+    res.redirect(`/technologies/${req.body.techId}`)
   });
 });
 
