b168ef7b55e0b938c02476a342f9dbe05bad5905
matt kersner <mattkersner@gmail.com>
2017-02-06 17:14:27 -0500

    fully functioning conditionally rendering nav bar

--- routes/categories.js
+++ routes/categories.js
@@ -8,7 +8,8 @@ router.get('/', function(req, res, next) {
   .then(function(categories) {
     res.render('categories/index', {
       title: 'Categories',
-      categories: categories
+      categories: categories,
+      user: req.user
     });
   });
 });
@@ -20,7 +21,8 @@ router.get('/Languages', function(req, res, next) {
     where: { category_name: 'Languages' }
   }).then((data) => {
       res.render('categories/Language', {
-      techs: data
+      techs: data,
+      user: req.user
     });
   });
 });
@@ -30,7 +32,8 @@ router.get('/Libraries', function(req, res, next) {
     where: { category_name: 'Libraries' }
   }).then((data) => {
     res.render('categories/library', {
-      techs: data
+      techs: data,
+      user: req.user
     });
   });
 });
@@ -40,7 +43,8 @@ router.get('/Frameworks', function(req, res, next) {
     where: { category_name: 'Frameworks' }
   }).then((data) => {
     res.render('categories/framework', {
-      techs: data
+      techs: data,
+      user: req.user
     });
   });
 });
@@ -50,7 +54,8 @@ router.get('/Styling', function(req, res, next) {
     where: { category_name: 'Styling' }
   }).then((data) => {
     res.render('categories/styling', {
-      techs: data
+      techs: data,
+      user: req.user
     });
   });
 });
@@ -60,7 +65,8 @@ router.get('/Modules_and_Components', function(req, res, next) {
     where: { category_name: 'Modules_and_Components' }
   }).then((data) => {
     res.render('categories/module', {
-      techs: data
+      techs: data,
+      user: req.user
     });
   });
 });
@@ -70,7 +76,8 @@ router.get('/Plugins', function(req, res, next) {
     where: { category_name: 'Plugins' }
   }).then((data) => {
     res.render('categories/plugins', {
-      techs: data
+      techs: data,
+      user: req.user
     });
   });
 });
@@ -80,7 +87,8 @@ router.get('/APIs', function(req, res, next) {
     where: { category_name: 'APIs' }
   }).then((data) => {
     res.render('categories/api', {
-      techs: data
+      techs: data,
+      user: req.user
     });
   });
 });
@@ -90,7 +98,8 @@ router.get('/Collaboration_Tools', function(req, res, next) {
     where: { category_name: 'Collaboration_Tools' }
   }).then((data) => {
     res.render('categories/collaboration', {
-      techs: data
+      techs: data,
+      user: req.user
     });
   });
 });
@@ -100,7 +109,8 @@ router.get('/Text_editors', function(req, res, next) {
     where: { category_name: 'Text_Editors' }
   }).then((data) => {
     res.render('categories/editors', {
-      techs: data
+      techs: data,
+      user: req.user
     });
   });
 });
@@ -110,7 +120,8 @@ router.get('/Productivity', function(req, res, next) {
     where: { category_name: 'Productivity' }
   }).then((data) => {
     res.render('categories/productivity', {
-      techs: data
+      techs: data,
+      user: req.user
     });
   });
 });
