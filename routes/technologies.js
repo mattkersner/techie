eb4aab31de501ff1f61769852e5045b9151dc6a9
mimi klein <lifeistoogoodtobetrue@gmail.com>
2017-02-06 20:32:23 -0500

    fixed duplicate favorite bug

--- routes/technologies.js
+++ routes/technologies.js
@@ -2,19 +2,19 @@ const express = require('express');
 const router = express.Router();
 const models = require('../db/models/index');
 const reviews = require('../reviews/reviews');
+const faveHelpers = require('../faves/fave-helpers');
 
 router.get('/', function(req, res, next) {
   models.Technology.findAll({}).then(function(technology) {
     res.render('technology/list', {
-      technology: technology,
-      user: req.user
+      technology: technology
     });
   });
 });
 
 router.get('/new', function(req, res, next) {
   res.render('technology/new', {
-  user: req.user
+
   });
 });
 
@@ -43,7 +43,7 @@ router.get('/:id', reviews.getReviews, function(req, res, next) {
 //render edit tech form
 router.get('/:id/edit', function(req, res, next) {
   models.Technology.findById(req.params.id).then(function(tech) {
-    res.render('technology/edit', { tech: tech, user: req.user });
+    res.render('technology/edit', { tech: tech });
   });
 });
 
@@ -60,14 +60,22 @@ router.put('/:id', function(req, res, next) {
   });
 });
 
-router.post('/favorites', function(req, res, next) {
-  models.Favorites.create({
+//post user's favorites to favorites table
+router.post('/favorites', faveHelpers.findFaves, function(req, res, next) {
+  //map through faves obj to extract id's, save result in idArr variable.
+    const idArr = res.locals.faves.map(data => data.id);
+    //check whether tech_id is = to an id already in user's favorites
+    if (!idArr.includes(parseInt(req.body.tech_id)))  {
+    models.Favorites.create({
     user_id: req.user.id, // properly passing user id
     tech_id: req.body.tech_id // pulling from views
   }).then(function() {
     res.redirect('/user');
-  });
-});
+   })
+  } else {
+    res.render('technology/error');
+    }
+ });
 
 router.delete('/user/:id', function(req, res, next) {
   models.Favorites.destroy({
@@ -87,7 +95,7 @@ router.delete('/:id/reviews/:rid', function(req, res) {
 
 router.get('/:id/reviews/:rid/edit', function(req, res) {
   models.Reviews.findById(req.params.rid).then(function(review) {
-  res.render('review/edit', { review: review, user: req.user });
+  res.render('review/edit', { review: review });
   });
 });
 
